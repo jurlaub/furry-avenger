fbac21650b995af5703f63828b3aed40b381fac8
jurlaub <j.urlaub@gmail.com>
2013-08-25 19:54:31 -0700

    cluster function - in process

--- Project4_TSP.py
+++ Project4_TSP.py
@@ -4,6 +4,63 @@ import getopt
 import os.path
 
 
+class Cluster():
+    # nearestNeighbers
+    # Cluster Max-Length
+    #  n
+    # 
+    #list of tuples, distances 
+    x = 0
+    y = 0
+    nearNeighbor = []
+    cityList = []
+    length = 0
+
+    def __init__(self, n, node, distance):
+        self.n = n
+        self.x = node[1]
+        self.y = node[2]
+        self.cityList.append([node, distance])
+
+    def addCityList(val):
+        self.cityList.append(val)
+
+
+
+
+
+# pass in distance (n from parentnode), 
+# list of clusters
+# 
+def make_add_Cluster(entry, ClusterList):
+
+    # look at list of clusters
+    # current node: find distance from each cluster parent node, 
+    # if within n, add 
+    # if not make new cluster
+
+    if not ClusterList:
+        #create new cluster
+        # find n from x or y      
+        n = max(entry[1], entry[2])
+        print n
+        ClusterList.append(Cluster(n, entry, 0))
+        #v = ClusterList[0]
+        #print v.cityList[0]
+        #v.cityList[0] = ["hello world", 8]
+        return
+
+    x1, y1 = entry[1], entry[2]
+
+    for item in ClusterList:
+        # x2, y2 = item.x, item.y
+        # print x2
+        # print y2
+        print item.cityList[0]
+
+
+    ClusterList.append(entry)
+
 
 
 def distance_matrix(coordinates):
@@ -20,23 +77,25 @@ def distance_matrix(coordinates):
     return distance
 
 
-def input_coords(filename):
+def input_coords(filename, ClusterList):
     '''This function reads city coordinates from a text file'''
-    runningList = []
-    fi = open(filename)
+    
+    fi = open(filename)   
     with fi as f:
         #add in something to skip first line, also first character of each line (the city number)
+    
         for line in f.readlines():
             n,x,y = line.strip().split(' ')
             #coordinates.append((float(x), float(y)))
-            print str(n) + "   " + str(x) + "   " + str(y)
+            #print str(n) + "   " + str(x) + "   " + str(y)
             entry = (n, x, y)
 
-            #call distance function
+            #call make_add_Cluster()
+            make_add_Cluster(entry, ClusterList)
 
-            runningList.append(entry)
+            #ClusterList.append(entry)
     fi.close()
-    return runningList
+    return ClusterList
 
 
 def myTour(matrix, tour):
@@ -49,9 +108,15 @@ def myTour(matrix, tour):
         total += matrix[city_i, city_j]
     return total
             
+# place for cluster optimization
+def command(filename):
 
+    ClusterList = []
 
+    #call input_coords ()
+    ClusterList = input_coords(filename, ClusterList)
 
+    #print ClusterList
 
 
  
@@ -77,7 +142,7 @@ def main():
 def process(arg):
     try:
         if os.path.exists(arg):
-            print input_coords(arg)
+            command(arg)
 
     except IOError as e:
         print("({})".format(e))
